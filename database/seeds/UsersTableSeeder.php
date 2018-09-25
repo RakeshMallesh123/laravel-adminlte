e3a3ee63cb612ad9fe85dc726ae5d36ef86132a2
Rakeesh <rakeshmallesh1@gmail.com>
2018-09-25 09:52:05 +0530

    Seed Database Modification

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -11,7 +11,7 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('admins')->insert([
+        DB::table('users')->insert([
             'name' => 'Rakesh',
 	        'email' => 'user@domain.com',
 	        'password' => bcrypt('123456'),
