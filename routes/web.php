080007a80ad90c1ea3139190f0bf3be83d6cd445
Rakeesh <rakeshmallesh1@gmail.com>
2018-09-25 09:35:44 +0530

    Admin login URL changes

--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,6 @@ Route::get('/home', 'HomeController@index')->name('home');
 Route::prefix('admin')->group(function() {
 	Route::get('/login', 'Auth\AdminLoginController@showLoginForm')->name('admin.login');
 	Route::post('user/login', 'Auth\AdminLoginController@login')->name('admin.login.submit');
-	Route::get('/', 'AdminController@index')->name('admin.dashboard');
+	Route::get('/', 'Auth\AdminLoginController@showLoginForm')->name('admin.dashboard');
 	Route::get('/dashboard', 'AdminController@index')->name('admin.dashboard');
 });
